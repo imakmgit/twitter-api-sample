550ff7b82a93ae6cb9dd3620d723ead2890f6e51
Abani Meher <ameher@codal.com>
2014-11-29 01:47:22 +0530

    Added file header and change include file name

--- twitter-api/include.php
+++ twitter-api/include.php
@@ -1,3 +1,12 @@
 <?php
-require_once 'TwitterConnector.php';
+/**
+ * File includes files required to use Twitter REST API
+ *
+ * @category API
+ * @package  Twitter_API_PHP
+ * @author   Abani Meher <abanimeher@gmail.com>
+ * @license  MIT License
+ * @link
+ */
+require_once 'TwitterClient.php';
 require_once 'TwitterApi.php';
